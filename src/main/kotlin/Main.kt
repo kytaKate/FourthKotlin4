a310d2d07c8bc6cff34e3fe6a43bf40b8deb36ff
Kate <@123456789katecat.com>
2022-02-18 18:20:57 +0200

    add part1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val a = readLine()!!.toInt()
+    when(a){
+//        5         -> println("C")
+//        99         -> println("A")
+//        45         -> println("A")
+//        235         -> println("B")
+//        1736         -> println("C")
+//        !in 7..15 -> println("Я таких не знаю")
+//        else      -> println("Я таких тем более не знаю")
+    }
+   val out = when {
+       a == 10  -> "a = 10"
+       a > 10   -> "a < 10"
+       else     -> "Not 5 not 6"
+   }
+    println(out)
 }
--- src/main/kotlin/part1/Sample.kt
+++ src/main/kotlin/part1/Sample.kt
@@ -5,15 +5,24 @@ package part1
 Гарантируется, что число положительное.
  */
 fun solution(a: Int): Char {
-    TODO("Not yet implemented")
-}
+    when (a) {
+        in 10..99 -> 'A'
+        in 100..999 -> 'B'
+        else -> return 'C'
+    }
 
-fun main() {
-    println(solution(5))     // C
-    println(solution(99))    // A
-    println(solution(45))    // A
-    println(solution(235))   // B
-    println(solution(1736))  // C
-}
+
+    fun main() {
+        println(solution(5))     // C
+        println(solution(99))    // A
+        println(solution(45))    // A
+        println(solution(235))   // B
+        println(solution(1736))  // C
+
+        val a = readLine()!!.toInt()
+        println(solution(a))
+    }
+
+
 
 
